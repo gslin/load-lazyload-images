9d8c9913fe90789da2561c88bcf3ca04be3ef058
Gea-Suan Lin <darkkiller@gmail.com>
2014-01-05 00:13:26 +0800

    Implement actual script to load data-original & data-src urls.

--- load-lazyload.js
+++ load-lazyload.js
@@ -1,2 +1,15 @@
 (function(){
+    var imgs = document.getElementsByTagName('imgs');
+    var imgs_size = imgs.length;
+    var url;
+
+    for (var i = 0; i < imgs_size; i++) {
+        var el = imgs[i];
+
+        if (url = el.getAttribute('data-original')) {
+            el.setAttribute('src', url);
+        } else if (url = el.getAttribute('data-src')) {
+            el.setAttribute('src', url);
+        }
+    }
 })();
