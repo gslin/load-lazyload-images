88504d7da14118d0d36acf3221e047776c0fd255
Gea-Suan Lin <gslin@gslin.org>
2024-12-28 02:22:07 +0800

    refactor: indent.

--- load-lazyload-images.js
+++ load-lazyload-images.js
@@ -1,42 +1,42 @@
 (() => {
-    'use strict';
+  'use strict';
 
-    let clean = function(el){
-        const imgs = el.querySelectorAll('img');
+  let clean = function(el){
+    const imgs = el.querySelectorAll('img');
 
-        for (const img of imgs) {
-            var url;
-
-            if (img.getAttribute('loading')) {
-                img.removeAttribute('loading');
-            }
-
-            if (url = img.getAttribute('data-original')) {
-                img.setAttribute('src', url);
-            } else if (url = img.getAttribute('data-src')) {
-                img.setAttribute('src', url);
-            } else if (url = img.getAttribute('file')) {
-                img.setAttribute('src', url);
-            }
-        }
-    };
-
-    // For new elements
-    let ob = new window.MutationObserver(muts => {
-        muts.forEach(mut => {
-            if (mut.type !== "childList") {
-                return;
-            }
-            clean(mut.target);
-        });
-    });
-    ob.observe(document, {
-        childList: true,
-        subtree: true,
-    });
-
-    const imgs = document.getElementsByTagName('img');
     for (const img of imgs) {
-        clean(img);
+      var url;
+
+      if (img.getAttribute('loading')) {
+        img.removeAttribute('loading');
+      }
+
+      if (url = img.getAttribute('data-original')) {
+        img.setAttribute('src', url);
+      } else if (url = img.getAttribute('data-src')) {
+        img.setAttribute('src', url);
+      } else if (url = img.getAttribute('file')) {
+        img.setAttribute('src', url);
+      }
     }
+  };
+
+  // For new elements
+  let ob = new window.MutationObserver(muts => {
+    muts.forEach(mut => {
+      if (mut.type !== "childList") {
+        return;
+      }
+      clean(mut.target);
+    });
+  });
+  ob.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+
+  const imgs = document.getElementsByTagName('img');
+  for (const img of imgs) {
+    clean(img);
+  }
 })();
